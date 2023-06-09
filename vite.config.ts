40b6475ae8b1b2e054eb0269618a55e3b3a3c095
Shiyan7 <jeka.shiyan@gmail.com>
2023-06-09 12:27:26 +0300

    fix: fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import svg from '@neodx/svg/vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
 // https://vitejs.dev/config/
@@ -12,7 +11,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    tsconfigPaths(),
     react(),
     svg({
       root: 'src/shared/ui/icon/assets',
